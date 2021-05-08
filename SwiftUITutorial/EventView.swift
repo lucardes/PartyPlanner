d0884408a4870051b07f4f520a010d597c41a3d9
lethycia maia <lethyciamaia@192.168.0.122>
2021-05-08 16:57:09 -0300

    eventView Number of people

--- SwiftUITutorial/EventView.swift
+++ SwiftUITutorial/EventView.swift
@@ -4,52 +4,54 @@ import SwiftUI
 struct EventView: View {
     
     @State var eventColor = Color(.systemRed)
-    @State var numOfPeople = 0
+    @State var numOfPeople = ""
     @StateObject var eventController = EventController()
     @State var emptyItem: Item = Item()
-    @State var itens: [Item] = testDataItensBebidas
-    @State var itens2: [Item] = testDataItensComidas
+    @State var beverages: [Item] = testDataItensBebidas
+    @State var foods: [Item] = testDataItensComidas
     @State private var showModal: Bool = false
     
     var body: some View {
         
-        VStack(alignment: .leading){
-            List {
-                
-                TextField("N de Participantes", value: $numOfPeople, formatter: NumberFormatter())
-                                        .keyboardType(.numberPad)
-                
-                Section(header: Text("Bebidas")){
-                    ForEach (itens.indices) { idx in
-                        ItemCellTotal(item: self.$itens[idx])
+        NavigationView {
+            VStack(alignment: .leading){
+                List {
+                    TextField("Nº de Convidados", value: $numOfPeople, formatter: NumberFormatter(), onEditingChanged: { (changed) in
+                    })
+                    .keyboardType(.numberPad)
+                    
+                    Section(header: Text("Bebidas")){
+                        ForEach (beverages.indices) { idx in
+                            ItemCellTotal(item: self.$beverages[idx])
+                        }
+                    }
+                    
+                    Section(header: Text("Comidas")){
+                        ForEach (foods.indices) { idx in
+                            ItemCellTotal(item: self.$foods[idx])
+                        }
                     }
                 }
+                .listStyle(GroupedListStyle())
                 
-                Section(header: Text("Comidas")){
-                    ForEach (itens2.indices) { idx in
-                        ItemCellTotal(item: self.$itens2[idx])
-                    }
+                Button(action: {
+                    self.showModal = true
+                }) {
+                  HStack {
+                    Image(systemName: "plus.circle.fill")
+                      .resizable()
+                      .frame(width: 20, height: 20)
+                    Text("Incluir item")
+                  }
+                }
+                .padding()
+                .accentColor(Color(UIColor.systemRed))
+                .sheet(isPresented: self.$showModal) {
+                    CreateItemView(color: $eventColor)
                 }
             }
-            .listStyle(GroupedListStyle())
-            
-            Button(action: {
-                self.showModal = true
-            }) {
-              HStack {
-                Image(systemName: "plus.circle.fill")
-                  .resizable()
-                  .frame(width: 20, height: 20)
-                Text("Incluir item")
-              }
-            }
-            .padding()
-            .accentColor(Color(UIColor.systemRed))
-            .sheet(isPresented: self.$showModal) {
-                CreateItemView(color: $eventColor)
-            }
+            .navigationTitle("Nome do evento") //event.name
         }
-        .navigationTitle("Lista de compras")
     }
 }
 
